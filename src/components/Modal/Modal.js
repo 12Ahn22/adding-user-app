046af9f1b7c374c3bc7e0a048c2b2aad4f26dfd0
12ayo22 <12ayo22@gmail.com>
2022-12-30 15:39:54 +0900

    [feat] 모달 컴포넌트 마크업 & 스타일 완성

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Button from '../Button/Button';
 import styles from './Modal.module.css';
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ isOpen, onCloseHandler }) => {
+  // if (!isOpen) return <></>;
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.modal}>
+        <h2 className={styles.title}>Invalid Input</h2>
+        <div className={styles.content}>
+          <div>Please enter a vallid age</div>
+          <Button text={'Okay'} onClickHandler={onCloseHandler} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
