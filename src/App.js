5885948b5841d855692e589a0df49276824e35ea
12ayo22 <12ayo22@gmail.com>
2022-12-30 14:23:23 +0900

    [feat] UserList 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import AddCard from './components/AddCard/AddCard';
+import UserList from './components/UserList/UserList';
 
 function App() {
   const [userList, setUserList] = useState([]);
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <AddCard setUserList={setUserList} userList={userList} />
+      <UserList />
     </div>
   );
 }
---
+++ src/components/UserList/UserList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './UserList.module.css';
+
+const UserList = () => {
+  return <div>UserList</div>;
+};
+
+export default UserList;
