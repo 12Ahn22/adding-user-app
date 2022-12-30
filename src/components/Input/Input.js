a593d047f9dbda2e29913bba43f1b30cded3fd35
12ayo22 <12ayo22@gmail.com>
2022-12-30 14:03:29 +0900

    [feat] 공용 Input 컴포넌트 기능 완성

---
+++ src/components/Input/Input.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+/* 
+  공통 Input 컴포넌트
+*/
+const Input = ({ label, value, onChangeHandler, type = 'text' }) => {
+  const id = `${label}-input`;
+  return (
+    <div>
+      <label className={styles.label} htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className={styles.input}
+        id={id}
+        value={value}
+        onChange={onChangeHandler}
+        type={type}
+      />
+    </div>
+  );
+};
+
+export default Input;
