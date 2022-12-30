215cd5d96e753c6118eab8a8776efda1d787ed83
12ayo22 <12ayo22@gmail.com>
2022-12-30 15:48:17 +0900

    [feat] 모달 기능 추가 - 외부 클릭 시, 모달창 닫히는 기능 추가 - 모달 title과 text를 외부에서 받도록 props 설정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import { useState } from 'react';
 import AddCard from './components/AddCard/AddCard';
+import Modal from './components/Modal/Modal';
 import UserList from './components/UserList/UserList';
 
 function App() {
   const [userList, setUserList] = useState([{ userName: 'ayo', age: '30' }]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalMsg, setModalMsg] = useState({
+    title: '',
+    text: '',
+  });
+  const closeModalClickHandler = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div>
       <AddCard setUserList={setUserList} userList={userList} />
       <UserList userList={userList} />
+
+      <Modal
+        isOpen={isOpen}
+        {...modalMsg}
+        onCloseHandler={closeModalClickHandler}
+      />
     </div>
   );
 }
